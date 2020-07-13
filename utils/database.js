b3fc09c227c173a2db5480d963eb6e3755faa8db
= <=>
2020-07-13 09:19:29 -0300

    Add, Remove, Update, Read methods added

--- utils/database.js
+++ utils/database.js
@@ -1,7 +1,7 @@
 import mysql from 'mysql'
 
 /*
-    * TODO: Add the [add, remove, read, update] methods
+    x TODO: Add the [add, remove, read, update] methods
     * TODO: Check if there is a way to create a db if it doesn't exist
     * TODO: Try to do a mini web version using http
     * TODO: Try to do a mini interface using Electron
@@ -21,7 +21,10 @@ class Database {
 
         this.connectDatabase()
 
-        this.readAll()
+        // this.readAll()
+        // this.add({ todo: 'This is awesome' })
+        this.read(1)
+        this.read(2)
     }
 
     connectDatabase() {
@@ -34,6 +37,42 @@ class Database {
 
     readAll() {
         const query = `SELECT * FROM ${this.tableName}`
+        this.database.query(query, (err, res) => {
+            if (err) throw err
+
+            console.log(res)
+        })
+    }
+
+    read(id = 1) {
+        const query = `SELECT * FROM ${this.tableName} WHERE id = ${id}`
+        this.database.query(query, (err, res) => {
+            if (err) throw err
+
+            console.log(res)
+        })
+    }
+
+    add(data) {
+        const query = `INSERT INTO ${this.tableName} SET ?`
+        this.database.query(query, data, (err, res) => {
+            if (err) throw err
+
+            console.log(res)
+        })
+    }
+
+    remove(id) {
+        const query = `DELETE FROM ${this.tableName} WHERE id = ${id}`
+        this.database.query(query, (err, res) => {
+            if (err) throw err
+
+            console.log(res)
+        })
+    }
+
+    update(id, data) {
+        const query = `UPDATE ${this.tableName} SET todo = '${data}' WHERE id = ${id}`
         this.database.query(query, (err, res) => {
             if (err) throw err
 
